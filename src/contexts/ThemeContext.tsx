fe35f2c6442a5eafdd373210aec68af9c45338d7
Abel Filipe Cardoso Candido <afcardosocandido@gmail.com>
2025-12-09 23:18:33 -0300

    Update ThemeContext.tsx

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 
+interface ThemeContextType {
+  theme: 'light' | 'dark';
+  toggle: () => void;
+}
 
-export const ThemeContext = React.createContext({
-theme: 'light',
-toggle: () => {},
+export const ThemeContext = React.createContext<ThemeContextType>({
+  theme: 'light',
+  toggle: () => {},
 });
 
-
 export const ThemeProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
-const [theme, setTheme] = React.useState<'light'|'dark'>('light');
-React.useEffect(() => {
-document.documentElement.setAttribute('data-theme', theme);
-}, [theme]);
-const toggle = () => setTheme(t => t === 'light' ? 'dark' : 'light');
-return <ThemeContext.Provider value={{theme, toggle}}>{children}</ThemeContext.Provider>;
+  const [theme, setTheme] = React.useState<'light'|'dark'>('light');
+  
+  React.useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+  
+  const toggle = () => setTheme(t => t === 'light' ? 'dark' : 'light');
+  
+  return (
+    <ThemeContext.Provider value={{theme, toggle}}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
-
 
 export default ThemeProvider;
