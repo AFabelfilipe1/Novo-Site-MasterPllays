a00cca631b912c9ea139ae7ef1b1a5033c05a2eb
Abel Filipe Cardoso Candido <afcardosocandido@gmail.com>
2025-12-09 23:31:13 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Novo-Site-MasterPlays/',
   plugins: [react()],
+  base: '/Novo-Site-MasterPflage/', // IMPORTANTE para GitHub Pages
 })
