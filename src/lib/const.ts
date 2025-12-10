ece90ab2f93a0b65317dd0920888d29d78b986c5
Abel Filipe Cardoso Candido <afcardosocandido@gmail.com>
2025-12-09 23:19:42 -0300

    Update const.ts

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,6 +1,13 @@
 export const APP_NAME = 'MasterPllays';
 export const ROUTES = {
-HOME: '/',
-WATCH: '/watch/:id',
-PROFILE: '/profile',
+  HOME: '/',
+  WATCH: (id: string) => `/watch/${id}`, // Agora é uma função
+  PROFILE: '/profile',
+  MOVIES: '/movies',
+  GAMES: '/games',
+  VLOGS: '/vlogs',
+  AUTH: '/auth',
+  ADMIN: '/admin',
+  ADMIN_UPLOAD: '/admin/upload',
+  ABOUT: '/about',
 };
