d35eaa9c8fe506f31c0ca03eb9bec83367b8496e
Abel Filipe Cardoso Candido <afcardosocandido@gmail.com>
2025-12-09 23:38:51 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Novo-Site-MasterPflage/', // IMPORTANTE para GitHub Pages
-})
+  base: '/Novo-Site-MasterPflage/', // EXATAMENTE como está no seu repositório
+});
