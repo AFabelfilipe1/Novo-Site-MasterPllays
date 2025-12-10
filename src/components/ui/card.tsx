0ad6da598e89313c63009c66c30e27bac1fb4c1c
Abel Filipe Cardoso Candido <afcardosocandido@gmail.com>
2025-12-09 23:17:56 -0300

    Update card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-
 export const Card: React.FC<React.PropsWithChildren<{className?: string;}>> = ({ children, className = '' }) => {
-return (
-<div className={`rounded-xl shadow-sm p-4 bg-white ${className}`}>
-{children}
-</div>
-);
+  return (
+    <div className={`rounded-xl shadow-sm p-4 bg-white ${className}`}>
+      {children}
+    </div>
+  );
 };
 
-
-export default Card;
+// Removi o export default para manter apenas export nomeado
