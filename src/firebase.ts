03daa27aa7dfea3e3507843c6d06769ff8e0a2b2
Abel Filipe Cardoso Cândido <afcardosocandido@gmail.com>
2025-12-24 17:39:53 -0300

    Update Firebase config with real credentials

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,22 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "your-app-id"
-}
+  apiKey: "AIzaSyBZqfKbDO4EP8ua3Sv1gOhNgY31gZPmHwU",
+  authDomain: "masterpllays.firebaseapp.com",
+  projectId: "masterpllays",
+  storageBucket: "masterpllays.firebasestorage.app",
+  messagingSenderId: "554976769966",
+  appId: "1:554976769966:web:4fc12441ac7ea490757a3f",
+  measurementId: "G-6MGFHZ7N76"
+};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app)
